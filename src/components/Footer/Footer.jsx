dd7cea63d08ece32dedd78cd52592918190b5d13
Alexander Murphy <alexanderjmurphy92@gmail.com>
2024-07-01 19:37:56 -0400

    Designed Landing Page

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "../Footer/Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__author">Developed by Alexander Murphy</p>
+      <p className="footer__date">June 2024</p>
+    </footer>
+  );
+}
+
+export default Footer;
