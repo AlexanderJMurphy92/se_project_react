54585608aba5eeb5aa0554b191935d5974905141
Alexander Murphy <alexanderjmurphy92@gmail.com>
2024-07-03 11:03:49 -0400

    Made Reveiwer Corrections

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,10 +16,12 @@ function App() {
   });
   const [activeModal, setActiveModal] = useState("");
   const [selectedCard, setSelectedCard] = useState({});
+
   const handleCardClick = (card) => {
     setActiveModal("preview");
     setSelectedCard(card);
   };
+
   const handleAddClick = () => {
     setActiveModal("add-garment");
   };
@@ -37,6 +39,8 @@ function App() {
       .catch(console.error);
   }, []);
 
+  const isModalOpen = activeModal === "add-garment";
+
   return (
     <div className="page">
       <div className="page__content">
@@ -47,11 +51,11 @@ function App() {
       <ModalWithForm
         title="New garment"
         buttonText="Add garment"
-        activeModal={activeModal}
+        isOpen={isModalOpen}
         onClose={closeActiveModal}
       >
         <label htmlFor="name" className="modal__label">
-          Name{" "}
+          Name
           <input
             type="text"
             className="modal__input"
@@ -60,7 +64,7 @@ function App() {
           />
         </label>
         <label htmlFor="imageUrl" className="modal__label">
-          Image{" "}
+          Image
           <input
             type="text"
             className="modal__input"
@@ -71,19 +75,37 @@ function App() {
         <fieldset className="modal__radio-buttons">
           <legend className="modal__legend">Select the weather type:</legend>
           <label htmlFor="hot" className="modal__label modal__label_type_radio">
-            <input id="Hot" type="radio" className="modal__radio-input" /> Hot
+            <input
+              id="Hot"
+              type="radio"
+              name="radio"
+              className="modal__radio-input"
+            />{" "}
+            Hot
           </label>
           <label
             htmlFor="warm"
             className="modal__label modal__label_type_radio"
           >
-            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+            <input
+              id="warm"
+              type="radio"
+              name="radio"
+              className="modal__radio-input"
+            />{" "}
+            Warm
           </label>
           <label
             htmlFor="cold"
             className="modal__label modal__label_type_radio"
           >
-            <input id="cold" type="radio" className="modal__radio-input" /> Cold
+            <input
+              id="cold"
+              type="radio"
+              name="radio"
+              className="modal__radio-input"
+            />{" "}
+            Cold
           </label>
         </fieldset>
       </ModalWithForm>
--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -2,27 +2,21 @@ import "./WeatherCard.css";
 import { defaultWeatherOptions, weatherOptions } from "../../utils/constants";
 
 function WeatherCard({ weatherData }) {
-  const filteredOptions = weatherOptions.filter((option) => {
-    return (
-      option.day === weatherData.isDay &&
-      option.condition === weatherData.condition
-    );
-  });
-
-  let weatherOption;
-  if (filteredOptions.length === 0) {
-    weatherOption = defaultWeatherOptions[weatherData.isDay ? "day" : "night"];
-  } else {
-    weatherOption = filteredOptions[0];
-  }
+  const weatherOption =
+    weatherOptions.find((option) => {
+      return (
+        option.day === weatherData.isDay &&
+        option.condition === weatherData.condition
+      );
+    }) || defaultWeatherOptions[weatherData.isDay ? "day" : "night"];
 
   return (
     <section className="weather-card">
       <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
       <img
-        src={weatherOption?.url}
-        alt={`Card showing ${weatherOption?.day ? "day" : "night"} ${
-          weatherOption?.condition
+        src={weatherOption.url}
+        alt={`Card showing ${weatherOption.day ? "day" : "night"} ${
+          weatherOption.condition
         } weather`}
         className="weather-card__image"
       />
